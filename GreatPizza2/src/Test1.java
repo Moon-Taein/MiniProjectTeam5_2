c762d45f47e7ec5f5dc336aff1f3a462a158895c
Moon-Taein <mji3988@gmail.com>
2023-06-22 13:17:47 +0900

    commit!

--- GreatPizza2/src/Test1.java
+++ GreatPizza2/src/Test1.java
@@ -2,5 +2,8 @@
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Hello World!2");
+		System.out.println("Hello World!3");
+		System.out.println("Hello World!4");
 	}
 }
