e820e27837b15d3a24b115074689a58fe45bef69
Moon-Taein <mji3988@gmail.com>
2023-06-22 21:12:44 +0900

    db연결 확인 commit

--- GreatPizza2/src/Test1.java
+++ GreatPizza2/src/Test1.java
@@ -1,8 +1,33 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		// 문태인 바보 ㅠㅠ
+
+		String sql = "select * from movie";
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DBUtil.getConnection();
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				String name = rs.getString("name");
+				System.out.println(name);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(rs);
+			DBUtil.close(stmt);
+			DBUtil.close(conn);
+		}
 
 	}
 }
