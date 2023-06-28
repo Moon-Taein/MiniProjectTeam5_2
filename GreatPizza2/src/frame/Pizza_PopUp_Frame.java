14bddd4e73c45d6cc7a6665b994b849078e481c8
Moon-Taein <51267486+Moon-Taein@users.noreply.github.com>
2023-06-28 15:52:08 +0900

    15:31 오후합치기 commit

--- GreatPizza2/src/frame/Pizza_PopUp_Frame.java
+++ GreatPizza2/src/frame/Pizza_PopUp_Frame.java
@@ -1,6 +1,8 @@
 package frame;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -23,9 +25,14 @@ public class Pizza_PopUp_Frame extends JDialog {
 
 	private ImagePanel contentPane;
 	private invisibility Utility = new invisibility();
+	private JButton sizeMButton;
+	private JButton sizeLButton;
 
 	// 피자_불고기피자M -> 불고기피자 라는 이름을 받아서 구현해보자
-	public Pizza_PopUp_Frame(String target, MainOrder mo) {
+	public Pizza_PopUp_Frame(String target, MainOrder mo, int detailOrderCount) {
+		System.out.println(mo.getOrderNumber());
+
+//		Font font = loadFont("BMJUA_ttf.ttf", Font.BOLD, 18);
 
 		setModal(true);
 		Sql_Methods sqm = new Sql_Methods();
@@ -42,18 +49,34 @@ public class Pizza_PopUp_Frame extends JDialog {
 		// image icon 사용해서 각 피자의 이미지 사용해서 넣어줘야함
 		ImageIcon ic = new ImageIcon(sqm.findPizzaImageMenuId("피자_" + target + "M"));
 
+		JLabel sizeLabel_L = new JLabel("L");
+		sizeLabel_L.setForeground(Color.WHITE);
+		sizeLabel_L.setFont(new Font("굴림", Font.PLAIN, 24));
+		sizeLabel_L.setBounds(617, 68, 57, 44);
+		sizeLabel_L.setOpaque(false);
+		contentPane.add(sizeLabel_L);
+
+		JLabel sizeLabel_M = new JLabel("M");
+		sizeLabel_M.setForeground(Color.WHITE);
+		sizeLabel_M.setFont(new Font("굴림", Font.PLAIN, 24));
+		sizeLabel_M.setBounds(460, 68, 25, 46);
+		sizeLabel_M.setOpaque(false);
+		contentPane.add(sizeLabel_M);
+
 		JLabel currentCount = new JLabel("1");
 		currentCount.setBounds(212, 491, 25, 15);
 		contentPane.add(currentCount);
 
 		JLabel sizeMPrice = new JLabel("원");
-		sizeMPrice.setBounds(522, 81, 57, 15);
+		sizeMPrice.setFont(new Font("굴림", Font.PLAIN, 22));
+		sizeMPrice.setBounds(500, 75, 100, 30);
 		// 피자M의 가격
 		sizeMPrice.setText(sqm.findPriceMenuId(target + "M") + "원");
 		contentPane.add(sizeMPrice);
 
 		JLabel sizeLPrice = new JLabel("원");
-		sizeLPrice.setBounds(679, 81, 57, 15);
+		sizeLPrice.setFont(new Font("굴림", Font.PLAIN, 22));
+		sizeLPrice.setBounds(655, 75, 100, 30);
 		// 피자L의 가격
 		sizeLPrice.setText(sqm.findPriceMenuId(target + "L") + "원");
 		contentPane.add(sizeLPrice);
@@ -61,26 +84,44 @@ public class Pizza_PopUp_Frame extends JDialog {
 		contentPane.add(imageLabel);
 
 		JLabel menu_name_Label = new JLabel("name");
-		menu_name_Label.setBounds(180, 30, 100, 30);
+		menu_name_Label.setFont(new Font("굴림", Font.PLAIN, 17));
+		menu_name_Label.setBounds(175, 40, 150, 30);
 		menu_name_Label.setText(target);
 		contentPane.add(menu_name_Label);
 
-		JButton sizeMButton = new JButton(
-				new ImageIcon(getClass().getClassLoader().getResource("popup\\pizzaSizeTrue.png")));
+		sizeMButton = new JButton(new ImageIcon(getClass().getClassLoader().getResource("popup\\pizzaSizeFalse.png")));
 		sizeMButton.setBounds(448, 67, 142, 46);
 		sizeMButton.setBorderPainted(false);
 		sizeMButton.setContentAreaFilled(false);
+		sizeMButton.setSelectedIcon(new ImageIcon(getClass().getClassLoader().getResource("popup\\pizzaSizeTrue.png")));
+		sizeMButton.setSelected(true);
+		sizeMButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sizeMButton.setSelected(true);
+				sizeLButton.setSelected(false);
+			}
+		});
 		contentPane.add(sizeMButton);
 
-		JButton sizeLButton = new JButton(
-				new ImageIcon(getClass().getClassLoader().getResource("popup\\pizzaSizeFalse.png")));
+		sizeLButton = new JButton(new ImageIcon(getClass().getClassLoader().getResource("popup\\pizzaSizeFalse.png")));
 		sizeLButton.setBounds(602, 67, 142, 46);
 		sizeLButton.setBorderPainted(false);
 		sizeLButton.setContentAreaFilled(false);
+		sizeLButton.setSelectedIcon(new ImageIcon(getClass().getClassLoader().getResource("popup\\pizzaSizeTrue.png")));
+		sizeLButton.setSelected(false);
+		sizeLButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sizeLButton.setSelected(true);
+				sizeMButton.setSelected(false);
+			}
+		});
 		contentPane.add(sizeLButton);
 
 		ButtonGroup edgeChoice = new ButtonGroup();
 
+		// radio버튼 구성
 		JRadioButton EdgeRadioButton_1 = new JRadioButton("기본");
 		EdgeRadioButton_1.setBounds(530, 335, 80, 23);
 		contentPane.add(EdgeRadioButton_1);
@@ -107,9 +148,11 @@ public class Pizza_PopUp_Frame extends JDialog {
 		EdgeRadioButton_4.setBorderPainted(true);
 		edgeChoice.add(EdgeRadioButton_4);
 
+		// 취소할 경우
 		JButton cancelButton = new JButton(new ImageIcon(getClass().getClassLoader().getResource("popup\\cancel.png")));
 		cancelButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+
 			}
 		});
 		cancelButton.setBounds(450, 490, 143, 46);
@@ -117,20 +160,30 @@ public class Pizza_PopUp_Frame extends JDialog {
 		cancelButton.setContentAreaFilled(false);
 		contentPane.add(cancelButton);
 
+		// 담기할 경우
 		JButton addButton = new JButton(new ImageIcon(getClass().getClassLoader().getResource("popup\\선택.png")));
+		addButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
 		addButton.setBounds(600, 490, 143, 46);
 		addButton.setBorderPainted(false);
 		addButton.setContentAreaFilled(false);
 		contentPane.add(addButton);
 
+		// 토핑 추가 부분
 		JLabel toppingPlus = new JLabel(new ImageIcon(getClass().getClassLoader().getResource("popup\\토핑추가.png")));
 		toppingPlus.setBounds(446, 130, 295, 162);
 		contentPane.add(toppingPlus);
 
+		// 엣지 변경 부분
 		JLabel changeEdge = new JLabel(new ImageIcon(getClass().getClassLoader().getResource("popup\\엣지변경.png")));
 		changeEdge.setBounds(446, 307, 295, 162);
 		contentPane.add(changeEdge);
 
+		// 수량 변경 부분
 		JLabel countLabel = new JLabel(new ImageIcon(getClass().getClassLoader().getResource("popup\\수량.png")));
 		countLabel.setBounds(80, 475, 273, 46);
 		contentPane.add(countLabel);
