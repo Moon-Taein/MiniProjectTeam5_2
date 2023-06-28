f9ca3538e223133deb0914bac5e91e6ea33c4a73
Ximen2464 <kang_001@naver.com>
2023-06-28 10:45:29 +0900

    메뉴리스트 버튼 생성

--- GreatPizza2/src/frame/MenuFrame.java
+++ GreatPizza2/src/frame/MenuFrame.java
@@ -1,5 +1,6 @@
 package frame;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
@@ -26,6 +27,7 @@ public class MenuFrame extends JFrame {
 	private JButton sideBtn;
 	private JButton drinkBtn;
 	private JButton makePizzaBtn;
+	private JLayeredPane menuPnl;
 
 	/**
 	 * Launch the application.
@@ -52,7 +54,12 @@ public class MenuFrame extends JFrame {
 
 		FrameSetting();
 
-		pizzaTapBtn();
+		menuPnl = new JLayeredPane();
+		menuPnl.setBackground(Color.WHITE);
+		menuPnl.setBounds(0, 196, 800, 469);
+		jlp.add(menuPnl, new Integer(3));
+
+		pizzaTabBtn();
 
 		buttonSetting();// 버튼 생성 메소드
 
@@ -60,13 +67,17 @@ public class MenuFrame extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				// 탭 전환시 지우고 다시 메뉴패널 위 버튼 생성
+				menuPnl.removeAll();
+				menuPnl.repaint();
+				pizzaTabBtn();
+
 				pizzaBtn.setIcon(icon.getwhitePizzaBtn());
 
 				sideBtn.setIcon(icon.getdarkSideBtn());
 				drinkBtn.setIcon(icon.getdarkDrinkBtn());
 				makePizzaBtn.setIcon(icon.getdarkMakePizzaBtn());
-
-				JLabel menulist = new JLabel();
+				// jlp.add(menuPnl, new Integer(3));
 
 			}
 		});
@@ -76,13 +87,16 @@ public class MenuFrame extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
+				menuPnl.removeAll();
+				menuPnl.repaint();
+				sideTabBtn();
+
 				sideBtn.setIcon(icon.getwhiteSideBtn());
 				// 버튼을 눌렀을 때 해당버튼 외 버튼들을 회색으로 바꿈
 				pizzaBtn.setIcon(icon.getdarkPizzaBtn());
 				drinkBtn.setIcon(icon.getdarkDrinkBtn());
 				makePizzaBtn.setIcon(icon.getdarkMakePizzaBtn());
-
-				System.out.println("사이드 탭 액션 진입");
+				menuPnl.repaint();
 
 			}
 		});
@@ -91,6 +105,11 @@ public class MenuFrame extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
+
+				menuPnl.removeAll();
+				menuPnl.repaint();
+				drinkTabBtn();
+
 				drinkBtn.setIcon(icon.getwhiteDrinkBtn());
 
 				pizzaBtn.setIcon(icon.getdarkPizzaBtn());
@@ -102,6 +121,11 @@ public class MenuFrame extends JFrame {
 
 		makePizzaBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				menuPnl.removeAll();
+				menuPnl.repaint();
+				MakePizzaTabBtn();
+				
 				pizzaBtn.setIcon(icon.getdarkPizzaBtn());
 				sideBtn.setIcon(icon.getdarkSideBtn());
 				drinkBtn.setIcon(icon.getdarkDrinkBtn());
@@ -114,10 +138,6 @@ public class MenuFrame extends JFrame {
 		util.invisible(sideBtn);
 		util.invisible(drinkBtn);
 		util.invisible(makePizzaBtn);
-
-		JLayeredPane menuPnl = new JLayeredPane();
-		menuPnl.setBounds(0, 665, 800, -468);
-		jlp.add(menuPnl, new Integer(2));
 
 	}
 
@@ -138,32 +158,30 @@ public class MenuFrame extends JFrame {
 		setSize(800, 900);
 		setLocationRelativeTo(null);
 
-		exiteKey();
-
 	}
 
 	private void buttonSetting() {
 
 		pizzaBtn = new JButton(icon.getwhitePizzaBtn());
-		pizzaBtn.setBounds(0, 150, 200, 50);
+		pizzaBtn.setBounds(0, 154, 200, 50);
 		jlp.add(pizzaBtn, new Integer(3));
-		// util.invisible(pizzaBtn);
+		util.invisible(pizzaBtn);
 
 		sideBtn = new JButton();
-		sideBtn.setBounds(200, 150, 200, 50);
+		sideBtn.setBounds(200, 154, 200, 50);
 		jlp.add(sideBtn, new Integer(3));
 
 		drinkBtn = new JButton();
-		drinkBtn.setBounds(400, 150, 200, 50);
+		drinkBtn.setBounds(400, 154, 200, 50);
 		jlp.add(drinkBtn, new Integer(3));
 
 		makePizzaBtn = new JButton();
-		makePizzaBtn.setBounds(600, 150, 200, 50);
+		makePizzaBtn.setBounds(600, 154, 200, 50);
 		jlp.add(makePizzaBtn, new Integer(3));
 
 	}
 
-	private void pizzaTapBtn() {
+	private void pizzaTabBtn() {
 		Sql_Methods sqm = new Sql_Methods();
 
 		// 버튼 눌렀을시 동작하는 메소드 (사이드, 음료용으로 바뀔듯 )
@@ -177,35 +195,139 @@ public class MenuFrame extends JFrame {
 			}
 		};
 
-		JButton btn1 = new JButton("불고기피자");
-		btn1.setBounds(50, 300, 200, 50);
+		JButton btn1 = new JButton(icon.getSmallBullgogiPizza());
+		btn1.setBounds(60, 70, 180, 150);
 		btn1.addActionListener(al);
-		jlp.add(btn1, new Integer(3));
+		menuPnl.add(btn1, new Integer(3));
+		util.invisible(btn1);
 
-		JButton btn2 = new JButton("페퍼로니피자");
-		btn2.setBounds(300, 300, 200, 50);
+		JButton btn2 = new JButton(icon.getSmallPepperoniPizza());
+		btn2.setBounds(300, 70, 180, 150);
 		btn2.addActionListener(al);
-		jlp.add(btn2, new Integer(3));
+		menuPnl.add(btn2, new Integer(3));
+		util.invisible(btn2);
 
-		JButton btn3 = new JButton("쉬림프피자");
-		btn3.setBounds(550, 300, 200, 50);
+		JButton btn3 = new JButton(icon.getSmallShrimpPizza());
+		btn3.setBounds(550, 70, 180, 150);
 		btn3.addActionListener(al);
-		jlp.add(btn3, new Integer(3));
+		menuPnl.add(btn3, new Integer(3));
+		util.invisible(btn3);
 
-		JButton btn4 = new JButton("치즈피자");
-		btn4.setBounds(50, 400, 200, 50);
+		JButton btn4 = new JButton(icon.getSmallCheesePizza());
+		btn4.setBounds(60, 260, 180, 150);
 		btn4.addActionListener(al);
-		jlp.add(btn4, new Integer(3));
+		menuPnl.add(btn4, new Integer(3));
+		util.invisible(btn4);
 
-		JButton btn5 = new JButton("콤비네이션피자");
-		btn5.setBounds(300, 400, 200, 50);
+		JButton btn5 = new JButton(icon.getSmallCombinationPizza());
+		btn5.setBounds(300, 260, 180, 150);
 		btn5.addActionListener(al);
-		jlp.add(btn5, new Integer(3));
+		menuPnl.add(btn5, new Integer(3));
+		util.invisible(btn5);
 
-		JButton btn6 = new JButton("포테이토피자");
-		btn6.setBounds(550, 400, 200, 50);
+		JButton btn6 = new JButton(icon.getSmallPotatoPizza());
+		btn6.setBounds(550, 260, 180, 150);
 		btn6.addActionListener(al);
-		jlp.add(btn6, new Integer(3));
+		menuPnl.add(btn6, new Integer(3));
+		util.invisible(btn6);
+
+	}
+
+	private void sideTabBtn() {
+		JButton btn1 = new JButton(icon.getCoke1());
+		btn1.setBounds(60, 70, 180, 150);
+		menuPnl.add(btn1, new Integer(3));
+		util.invisible(btn1);
+
+		JButton btn2 = new JButton(icon.getCider1());
+		btn2.setBounds(300, 70, 180, 150);
+		menuPnl.add(btn2, new Integer(3));
+		util.invisible(btn2);
+
+		JButton btn3 = new JButton(icon.getFanta1());
+		btn3.setBounds(550, 70, 180, 150);
+		menuPnl.add(btn3, new Integer(3));
+		util.invisible(btn3);
+
+		JButton btn4 = new JButton(icon.getCoke2());
+		btn4.setBounds(60, 260, 180, 150);
+		menuPnl.add(btn4, new Integer(3));
+		util.invisible(btn4);
+
+		JButton btn5 = new JButton(icon.getCider2());
+		btn5.setBounds(300, 260, 180, 150);
+		menuPnl.add(btn5, new Integer(3));
+		util.invisible(btn5);
+
+		JButton btn6 = new JButton(icon.getFanta2());
+		btn6.setBounds(550, 260, 180, 150);
+		menuPnl.add(btn6, new Integer(3));
+		util.invisible(btn6);
+	}
+
+	private void drinkTabBtn() {
+		JButton btn1 = new JButton(icon.getCoke1());
+		btn1.setBounds(60, 70, 180, 150);
+		menuPnl.add(btn1, new Integer(3));
+		util.invisible(btn1);
+
+		JButton btn2 = new JButton(icon.getCider1());
+		btn2.setBounds(300, 70, 180, 150);
+		menuPnl.add(btn2, new Integer(3));
+		util.invisible(btn2);
+
+		JButton btn3 = new JButton(icon.getFanta1());
+		btn3.setBounds(550, 70, 180, 150);
+		menuPnl.add(btn3, new Integer(3));
+		util.invisible(btn3);
+
+		JButton btn4 = new JButton(icon.getCoke2());
+		btn4.setBounds(60, 260, 180, 150);
+		menuPnl.add(btn4, new Integer(3));
+		util.invisible(btn4);
+
+		JButton btn5 = new JButton(icon.getCider2());
+		btn5.setBounds(300, 260, 180, 150);
+		menuPnl.add(btn5, new Integer(3));
+		util.invisible(btn5);
+
+		JButton btn6 = new JButton(icon.getFanta2());
+		btn6.setBounds(550, 260, 180, 150);
+		menuPnl.add(btn6, new Integer(3));
+		util.invisible(btn6);
+	}
+
+	private void MakePizzaTabBtn() {
+
+		JButton btn1 = new JButton(icon.getCoke1());
+		btn1.setBounds(60, 70, 180, 150);
+		menuPnl.add(btn1, new Integer(3));
+		util.invisible(btn1);
+
+		JButton btn2 = new JButton(icon.getCider1());
+		btn2.setBounds(300, 70, 180, 150);
+		menuPnl.add(btn2, new Integer(3));
+		util.invisible(btn2);
+
+		JButton btn3 = new JButton(icon.getFanta1());
+		btn3.setBounds(550, 70, 180, 150);
+		menuPnl.add(btn3, new Integer(3));
+		util.invisible(btn3);
+
+		JButton btn4 = new JButton(icon.getCoke2());
+		btn4.setBounds(60, 260, 180, 150);
+		menuPnl.add(btn4, new Integer(3));
+		util.invisible(btn4);
+
+		JButton btn5 = new JButton(icon.getCider2());
+		btn5.setBounds(300, 260, 180, 150);
+		menuPnl.add(btn5, new Integer(3));
+		util.invisible(btn5);
+
+		JButton btn6 = new JButton(icon.getFanta2());
+		btn6.setBounds(550, 260, 180, 150);
+		menuPnl.add(btn6, new Integer(3));
+		util.invisible(btn6);
 
 	}
 
--- GreatPizza2/src/img/imageIcon.java
+++ GreatPizza2/src/img/imageIcon.java
@@ -40,5 +40,47 @@ public class imageIcon {
 	public ImageIcon getdarkMakePizzaBtn() {
 		return new ImageIcon(getClass().getClassLoader().getResource("darkMakePizzaBtn.png"));
 	}
+	public ImageIcon getSmallBullgogiPizza() {
+		return new ImageIcon(getClass().getClassLoader().getResource("불고기.png"));
+	}
+	public ImageIcon getSmallPepperoniPizza() {
+		return new ImageIcon(getClass().getClassLoader().getResource("페퍼로니.png"));
+	}
+	public ImageIcon getSmallShrimpPizza() {
+		return new ImageIcon(getClass().getClassLoader().getResource("쉬림프.png"));
+	}
+	public ImageIcon getSmallCheesePizza() {
+		return new ImageIcon(getClass().getClassLoader().getResource("치즈.png"));
+	}
+	public ImageIcon getSmallCombinationPizza() {
+		return new ImageIcon(getClass().getClassLoader().getResource("콤비네이션.png"));
+	}
+	public ImageIcon getSmallPotatoPizza() {
+		return new ImageIcon(getClass().getClassLoader().getResource("포테이토.png"));
+	}
+
+	public ImageIcon getCoke1() {
+		return new ImageIcon(getClass().getClassLoader().getResource("콜라큰거.png"));
+	}
+	public ImageIcon getCoke2() {
+		return new ImageIcon(getClass().getClassLoader().getResource("콜라작은거.png"));
+	}
+
+	public ImageIcon getCider1() {
+		return new ImageIcon(getClass().getClassLoader().getResource("사이다큰거.png"));
+	}
+
+	public ImageIcon getCider2() {
+		return new ImageIcon(getClass().getClassLoader().getResource("사이다작은거.png"));
+	}
+
+	public ImageIcon getFanta1() {
+		return new ImageIcon(getClass().getClassLoader().getResource("환타큰거.png"));
+	}
+
+	public ImageIcon getFanta2() {
+		return new ImageIcon(getClass().getClassLoader().getResource("환타작은거.png"));
+	}
+	
 
 }
