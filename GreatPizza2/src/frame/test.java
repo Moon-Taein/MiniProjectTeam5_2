540bbe53d3721eb8d50e6166c9437e766a7f9002
Ximen2464 <kang_001@naver.com>
2023-11-06 18:43:07 +0900

    Pizza-Kiosk

--- GreatPizza2/src/frame/test.java
+++ GreatPizza2/src/frame/test.java
@@ -15,7 +15,7 @@ public class test extends JFrame {
 	private JPanel contentPane;
 
 	/**
-	 * Launch the application.
+	 *Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
